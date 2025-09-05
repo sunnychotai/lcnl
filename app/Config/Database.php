cdce8e506086d9c88168982e7c59f774ba6f6400
Sunny Chotai <sunnychotai@me.com>
2025-09-05 21:29:08 +0100

    DatabaseSetup-hostupdate

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => 'srv582.hstgr.io',
+        'username'     => 'u485920306_lcnl',
+        'password'     => 'L0h4n4!!',
+        'database'     => 'u485920306_lcnl',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
