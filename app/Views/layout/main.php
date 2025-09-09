99fc1e74f8dfdbb0a663120e7611a951f9e8e4c5
Sunny Chotai <sunnychotai@me.com>
2025-09-09 13:14:14 +0100

    css fix

--- app/Views/layout/main.php
+++ app/Views/layout/main.php
@@ -6,7 +6,7 @@
   <title><?= esc($title ?? 'LCNL') ?></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
-  <link rel="stylesheet" href="<?= base_url('assets/css/style.css?v=<?= time() ?>') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/css/lcnl.css?v=<?= time() ?>') ?>">
 
 </head>
 <body>
