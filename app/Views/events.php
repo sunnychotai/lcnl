657b5fea43d6b6193496c12081f1b40213ad3a5f
Sunny Chotai <sunnychotai@me.com>
2025-09-11 16:00:07 +0100

    debug 3

--- app/Views/events.php
+++ app/Views/events.php
@@ -1,12 +1,11 @@
 <?= $this->extend('layout/main') ?>
 <?= $this->section('content') ?>
-
 <!-- Hero Banner -->
 <div class="hero hero-rangoli-orange d-flex align-items-center justify-content-center">
   <div class="overlay"></div>
   <div class="container position-relative text-center">
-    <h1 class="text-white fw-bold">Events</h1>
-    <p class="text-white-75">Discover our upcoming events by month.</p>
+    <h1 class="text-white fw-bold">Gallery</h1>
+    <p class="text-white-75"></p>
   </div>
 </div>
 
