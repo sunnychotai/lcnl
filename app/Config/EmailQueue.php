4eb539bf8b2492306aa8853f4961d1d7a16b9dce
Sunny Chotai <sunnychotai@me.com>
2025-12-12 08:41:29 +0000

    1000 throttle

--- app/Config/EmailQueue.php
+++ app/Config/EmailQueue.php
@@ -8,8 +8,8 @@ class EmailQueue extends BaseConfig
 {
     public bool $sendEnabled = true;           // default; overridden by .env
     public bool $dryRun = false;          // default; overridden by .env
-    public int $batchSize = 500;
-    public int $dailyLimit = 5000;            // safety cap
+    public int $batchSize = 100;
+    public int $dailyLimit = 950;            // safety cap
     public $perMinuteLimit = 10;    // max 10 emails per minute
     public $sendDelayMs = 1000;  // 200ms delay between emails
 
