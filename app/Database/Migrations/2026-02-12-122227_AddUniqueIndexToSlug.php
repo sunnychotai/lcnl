aef47a3a9bf3da2b2113f75a3116062f0c8a7aef
Sunny Chotai <sunnychotai@me.com>
2026-02-12 12:25:43 +0000

    slug 2

--- app/Database/Migrations/2026-02-12-122227_AddUniqueIndexToSlug.php
+++ app/Database/Migrations/2026-02-12-122227_AddUniqueIndexToSlug.php
@@ -8,10 +8,17 @@ class AddUniqueIndexToSlug extends Migration
 {
     public function up()
     {
-        $this->forge->addKey('slug', true);
+        $this->forge->addColumn('events', [
+            'slug' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addUniqueKey('slug');
         $this->forge->processIndexes('events');
     }
-
     public function down()
     {
         $this->forge->dropKey('events', 'slug');
